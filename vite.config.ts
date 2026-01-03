2b986764df3429fa83cf027de7f99892cc5f5130
moonhwan2 <xelloers@gmail.com>
2026-01-03 23:04:21 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  }
+  base: '/engtothemoon/',
 })
